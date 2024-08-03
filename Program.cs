94e951d8302db4b88df8fc43662d2ecb789c2b3d
erickdavestech <erickdavestech@gmail.com>
2024-08-03 18:46:50 -0400

    Service for ApiDbContext-Connection to The SqlServer

--- Program.cs
+++ Program.cs
@@ -1,25 +1,25 @@
+using BACKEND_ASP.NET_WEB_API.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+// Services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Service for ApiDbContext
+builder.Services.AddDbContext<ApiDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnectionString")));
+// end Services
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
