bdeb2a2cd73d3941f14b6da6f25744ce9cfcba65
erickdavestech <erickdavestech@gmail.com>
2024-08-03 17:25:32 -0400

    _03_Models_Users_

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BACKEND_ASP.NET_WEB_API.Models
+{
+    public class Users
+    {
+        [Key]
+        public int userId{ get; set; }
+        [Required]
+        public string name { get; set; } = "";
+        [Required]
+        public string address { get; set; } = "";
+    }
+
+}
